f1436e358fa727a511dfe1b726492aa8fac55fa4
Zsolt <zsolt.iachmanovski@yahoo.com>
2023-03-28 17:27:58 +0300

    first commit

---
+++ src/test/java/RequestObject/RequestMethodType.java
@@ -0,0 +1,8 @@
+package RequestObject;
+
+public class RequestMethodType {
+    public static String GET_METHOD = "get";
+    public static String POST_METHOD = "post";
+    public static String PUT_METHOD = "put";
+    public static String DELETE_METHOD = "delete";
+}
---
+++ src/test/java/ResponseObject/ResponseBodyType.java
@@ -0,0 +1,6 @@
+package ResponseObject;
+
+public class ResponseBodyType {
+    public static String RESPONSE_LOGIN = "Login";
+    public static String RESPONSE_REGISTER = "Register";
+}
